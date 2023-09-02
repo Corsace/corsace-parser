e4b54a439eddddbe99218009960770d39eddd1cf
riesha <seleneia@outlook.jp>
2023-09-02 16:51:13 +0200

    wip docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
+//! wasm osu replay and beatmap parser
+
 pub mod beatmap;
 pub mod macros;
 pub mod replay;
 
 mod utils;
 
-use beatmap::{ParserBeatmap, ParserBeatmapAttributes, ParserDifficulty, ParserStrains};
-
+use beatmap::{
+    objects::HitObject, ParserBeatmap, ParserBeatmapAttributes, ParserDifficulty, ParserStrains,
+};
 use wasm_bindgen::prelude::*;
 
 use crate::{beatmap::ParserScoreState, replay::Replay};
@@ -17,10 +20,17 @@ extern "C" {
     fn log(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() { alert("Hello, replay-parser!"); }
-
-//^ returns js object containing replay metadata
+/// Parses the provided replay for basic information.
+/// # Example
+///
+/// ```
+///  const reader = new FileReader();
+///
+///  reader.onloadend = (evt) => {
+///    const replaydata = new Uint8Array(evt.target.result);
+///    console.log(wasm.parseReplay(replaydata));
+/// }
+/// ```
 #[wasm_bindgen(js_name = parseReplay)]
 pub fn parse_replay(replay: &mut [u8]) -> Result<Replay, JsError>
 {
@@ -28,7 +38,19 @@ pub fn parse_replay(replay: &mut [u8]) -> Result<Replay, JsError>
     Ok(parsed)
 }
 
-//^ return js object containing everything from parse_replay + replaydata as an array of hitobjects w hit/cursor/tap info
+/// Parses the provided replay for advanced information.
+///
+/// Includes everything from [`parse_replay`] plus raw replay data and replay frame data containing cursor and button information.
+/// # Example
+///
+/// ```
+///  const reader = new FileReader();
+///
+///  reader.onloadend = (evt) => {
+///    const replaydata = new Uint8Array(evt.target.result);
+///    console.log(wasm.parseReplayExtra(replaydata, beatmapdata));
+/// }
+/// ```
 #[wasm_bindgen(js_name = parseReplayExtra)]
 pub fn parse_replay_extra(replay: &mut [u8], beatmap: &mut [u8]) -> Result<Replay, JsError>
 {
@@ -36,7 +58,17 @@ pub fn parse_replay_extra(replay: &mut [u8], beatmap: &mut [u8]) -> Result<Repla
     Ok(extras)
 }
 
-//^ returns js object containing map metadata
+/// Parses the provided beatmap for basic information.
+/// # Example
+///
+/// ```
+///  const reader = new FileReader();
+///
+///  reader.onloadend = (evt) => {
+///    const beatmapdata = new Uint8Array(evt.target.result);
+///    console.log(wasm.parseBeatmap(beatmapdata));
+/// }
+/// ```
 #[wasm_bindgen(js_name = parseBeatmap)]
 pub fn parse_beatmap(beatmap: &mut [u8]) -> Result<ParserBeatmap, JsError>
 {
@@ -54,15 +86,38 @@ pub struct ParserScore
     pub clock_rate:     Option<f64>, //? if theres any rate changes in the pool this could be used to calc pp from the original map
     pub accuracy:       f64,
 }
-
-//^ return js object containing aim/speed pp and sr + total, need to figure out how to pass ar/od/cs/hp if there are any non-osu-mod modifications from the base map
+/// Parses the provided beatmap for advanced information.
+///
+/// Includes everything from [`parse_beatmap`] plus an array of all [`HitObject`].
+/// # Example
+///
+/// ```
+///  const reader = new FileReader();
+///
+///  reader.onloadend = (evt) => {
+///    const beatmapdata = new Uint8Array(evt.target.result);
+///    console.log(wasm.parseBeatmapExtra(beatmapdata));
+/// }
+/// ```
 #[wasm_bindgen(js_name = parseBeatmapExtra)]
 pub fn parse_beatmap_extra(beatmap: &mut [u8]) -> Result<ParserBeatmap, JsError>
 {
     let parsed = ParserBeatmap::parse_extra(&mut beatmap.as_ref())?;
     Ok(parsed)
 }
-
+/// Parses the provided beatmap and calculates difficulty and performance attributes.
+///
+/// If a [`ParserScore`] is provided, it will use the provided score state for calculation.
+/// # Example
+///
+/// ```
+///  const reader = new FileReader();
+///
+///  reader.onloadend = (evt) => {
+///    const beatmapdata = new Uint8Array(evt.target.result);
+///    console.log(wasm.parseBeatmapAttributes(beatmapdata));
+/// }
+/// ```
 #[wasm_bindgen(js_name = parseBeatmapAttributes)]
 pub fn parse_beatmap_attributes(
     score: Option<ParserScore>, beatmap: &mut [u8],
@@ -73,7 +128,19 @@ pub fn parse_beatmap_attributes(
         &mut beatmap.as_ref(),
     )?)
 }
-
+/// Parses the provided beatmap and calculates an array of gradual difficulty and optionally performance strains.
+///
+/// If an array of [`ParserScoreState`] is provided, it will also calculate gradual performance strains.
+/// # Example
+///
+/// ```
+///  const reader = new FileReader();
+///
+///  reader.onloadend = (evt) => {
+///    const beatmapdata = new Uint8Array(evt.target.result);
+///    console.log(wasm.parseBeatmapStrains(beatmapdata,scorearray));
+/// }
+/// ```
 #[wasm_bindgen(js_name = parseBeatmapStrains)]
 pub fn parse_beatmap_strains(
     beatmap: &mut [u8], score_states: Option<Vec<JsValue>>, mods: Option<u32>,
@@ -96,6 +163,6 @@ pub fn parse_beatmap_strains(
         mods,
     )?)
 }
-
+/// Call on init for better panic reports when debugging, not required.
 #[wasm_bindgen]
 pub fn init_panic_hook() { utils::set_panic_hook(); }
